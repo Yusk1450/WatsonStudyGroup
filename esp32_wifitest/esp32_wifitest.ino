141926e6d68743f9b1648405cd69c87f7b9b6ee7
Yusk <yusk1450@gmail.com>
2018-08-27 16:13:42 +0900

    Initial commit

---
+++ esp32_wifitest/esp32_wifitest.ino
@@ -0,0 +1,120 @@
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WifiMulti.h>
+
+#include <HTTPClient.h>
+
+const int btnPin = 34;
+int btnStateOld = LOW;
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+WiFiMulti wifiMulti;
+
+const char ssid[] = "ssid";              // SSID
+const char pwd[] = "Password";          // パスワード
+
+/* ----------------------------------------------------
+ * 初期化処理
+---------------------------------------------------- */
+void setup()
+{
+    pinMode(btnPin, INPUT);
+    Serial.begin(9600);
+    delay(200);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    delay(1000);
+    display.clearDisplay();
+
+    // Wifi接続
+    wifiMulti.addAP(ssid, pwd);
+    while (wifiMulti.run() != WL_CONNECTED)
+    {
+        Serial.println("Wifi connecting...");
+        drawText("Wifi connecting...");
+        delay(500);
+    }
+    Serial.println("Wifi connecting...OK!");
+    drawText("Wifi connecting...OK!");
+}
+
+/* ----------------------------------------------------
+ * メイン処理
+---------------------------------------------------- */
+void loop()
+{
+    // ボタン状態
+    int btnState = digitalRead(btnPin);
+
+    // ボタンが押されたとき
+    if (btnState == HIGH && btnStateOld == LOW)
+    {
+        Serial.println("Button Pressed");
+
+        // 
+        if (requestGetHttp("http://49.212.141.66/DENSAN_home/DENSAN_home.php"))
+        {
+            Serial.println("Success!");
+            drawText("Success!");
+        }
+        else
+        {
+            Serial.println("Failed: HTTP GET Request");
+            drawText("Failed!");
+        }
+
+        delay(10);      // チャタリング防止
+    }
+
+    btnStateOld = btnState;
+}
+
+void drawText(String text)
+{
+    display.clearDisplay();
+    delay(200);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+//    display.setTextColor(BLACK, WHITE);
+    
+    display.println(text);
+    display.display();
+}
+
+boolean requestGetHttp(String url)
+{
+    boolean succeed = false;
+
+    if (wifiMulti.run() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        http.begin(url);
+
+        int httpCode = http.GET();
+        Serial.println(httpCode);
+
+        if (httpCode > 0)
+        {
+            if (httpCode == HTTP_CODE_OK)
+            {
+                succeed = true;
+            }
+        }
+
+        http.end();
+    }
+
+    delay(100);
+    return succeed;
+}
+
